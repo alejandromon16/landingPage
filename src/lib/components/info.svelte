3d75f575aeae356aa9fa58c3b0f8f88f9114f56c
alejandromon16 <alejandromontero1551@gmail.com>
2022-05-23 16:37:28 -0400

    adding video

--- src/lib/components/info.svelte
+++ src/lib/components/info.svelte
@@ -10,7 +10,9 @@
 >
     <div class="animate__animated animate__backInRight">
         <h1 class="hover:underline-offset-8">{data.title}:</h1>
-        <h1 class="font-bold text-lg hover:underline-offset-8 hover:scale-110 duration-500">{data.info}</h1>
+        <a href={data.info.url}>
+             <h1 class="font-bold text-lg hover:underline-offset-8 hover:scale-110 duration-500">{data.info.name}</h1>
+        </a>
         <h1>{data.info2}</h1>
     </div>
 	
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,7 @@
 <div class="absolute w-full pb-40">
 	<div class="grid grid-cols-1 justify-items-center text-white ">
         <div class="mt-10 lg:mt-40 text-center animate__animated animate__zoomIn">
-            <h1  class=" text-7xl">London St</h1>
+            <h1  class="font-bold text-7xl">London St</h1>
             <h1 class=" text-3xl mt-10  animate__animated animate__zoomIn animate__delay-1s">A Sensible excuse</h1>
             <h1 class=" text-xl mt-10  animate__animated animate__zoomIn animate__delay-1s">Since July 13th, 1985</h1>
         </div>
@@ -11,6 +11,10 @@
             {#each datas as data}
                  <Info {data}/>
             {/each}
+        </div>
+
+        <div class="aspect-w-16 aspect-h-10 mt-[20%]">
+            <iframe src="https://www.youtube.com/embed/g3zRQXK7zoc" class="rounded" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
         </div>
     </div>
 </div>
@@ -21,17 +25,25 @@
     let datas = [
         {
             title: 'Locations',
-            info: 'Santa Cruz',
-            info2: 'La Paz/Cochabamba/Oruro'
-        },
-        {
-            title: 'Contacts',
-            info: 'Info@londonst.net',
+            info: {
+                name: 'Santa Cruz/La Paz/Cochabamba/Oruro',
+                url: 'https://api.whatsapp.com/send/?phone=59169079099&text&app_absent=0'
+            },
             info2: '(591)690-790-99'
         },
         {
+            title: 'Contacts',
+            info: {
+                name: 'Info@londonst.net'
+            },
+            info2: ''
+        },
+        {
             title: 'Follow us',
-            info: 'fb:londonst',
+            info: {
+                url: 'https://www.facebook.com/London-St-108841951007240/',
+                name: 'fb:londonst'
+            },
             info2: 'Ig:Londonst'
         },
     ]
