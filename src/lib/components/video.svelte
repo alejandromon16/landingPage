31f694504abd1edcb63dad80f83c38b6342d5615
alejandromon16 <alejandromontero1551@gmail.com>
2022-06-01 20:46:40 -0400

    link

--- src/lib/components/video.svelte
+++ src/lib/components/video.svelte
@@ -1,7 +1,13 @@
-<div class="">
-    <iframe src={url} class="aspect-video" frameborder="5px" allow=" fullscreen; picture-in-picture" allowfullscreen></iframe>
-</div>
-
 <script>
-    export let url;
+	export let url;
 </script>
+
+<div class="">
+	<iframe
+		src={url}
+		class="aspect-video"
+		frameborder="5px"
+		allow=" fullscreen; picture-in-picture"
+		allowfullscreen
+	/>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -33,7 +33,7 @@
 			},
 			info2: {
 				name: 'Tester London',
-				url: 'https://docs.google.com/forms/d/1mnCucDm8GLgIr7rqLPx2i-sCodf9BUApMBAAMFb-BYk/edit'
+				url: 'https://forms.gle/HM7crWWzSeHuVqyX7'
 			}
 		}
 	];
@@ -51,7 +51,9 @@
 />
 <div class="fixed top-[230px] left-0 right-0 bottom-0 lg:inset-0 -z-0 bg-black lg:bg-opacity-20" />
 <div class="absolute ">
-	<a href= "#inicio"class="fixed bottom-10 right-10 z-10 bg-white rounded-full p-3 lg:right-[100px] lg:bottom-[70px]"
+	<a
+		href="#inicio"
+		class="fixed bottom-10 right-10 z-10 bg-white rounded-full p-3 lg:right-[100px] lg:bottom-[70px]"
 		><svg
 			xmlns="http://www.w3.org/2000/svg"
 			class="h-6 w-6"
@@ -65,7 +67,8 @@
 				stroke-linejoin="round"
 				d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
 			/>
-		</svg></a>
+		</svg></a
+	>
 	>
 </div>
 <div class="absolute scroll-smooth">
@@ -75,14 +78,18 @@
 				class="grid grid-cols-1 justify-items-center text-white pt-[92px] lg:mt-40 text-center animate__animated animate__zoomIn"
 			>
 				<h1 class="font-bold text-6xl lg:text-9xl">London St</h1>
-				<h1 class=" text-2xl lg:text-5xl animate__animated animate__zoomIn animate__delay-1s lg:mt-10">
+				<h1
+					class=" text-2xl lg:text-5xl animate__animated animate__zoomIn animate__delay-1s lg:mt-10"
+				>
 					A Sensible excuse
 				</h1>
 				<h1 class=" text-2xl animate__animated animate__zoomIn animate__delay-1s lg:mt-10">
 					Since July 13th, 1985
 				</h1>
 			</div>
-			<div class="grid grid-cols-1 gap-y-20 lg:grid-cols-3 lg:gap-60 mt-20 overflow-hidden px-10 pb-10">
+			<div
+				class="grid grid-cols-1 gap-y-20 lg:grid-cols-3 lg:gap-60 mt-20 overflow-hidden px-10 pb-10"
+			>
 				{#each datas as data}
 					<Info {data} />
 				{/each}
